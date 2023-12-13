34885bd6f4340d56de86a524868e740261019b91
polarzero <0xpolarzero@gmail.com>
2023-12-13 13:57:08 +0100

    Add example

--- components/pages/home/optimizer/index.tsx
+++ components/pages/home/optimizer/index.tsx
@@ -3,11 +3,23 @@ import { type FC, useState } from 'react';
 import ImportsInput from './imports-input';
 import Recommendations from './recommendations';
 
+import { EXAMPLE_CODE } from '@/lib/constants/example';
+
+import { Button } from '@/components/ui';
+
 const Optimizer: FC = () => {
   const [input, setInput] = useState<string>('');
 
   return (
     <div className="flex flex-col space-y-4">
+      <div className="flex space-x-2 self-end">
+        <Button intent="primary" disabled={input !== ''} onClick={() => setInput(EXAMPLE_CODE)}>
+          Try with an example
+        </Button>
+        <Button variant="ghost" onClick={() => setInput('')}>
+          Clear
+        </Button>
+      </div>
       <ImportsInput input={input} setInput={setInput} />
       <Recommendations input={input} />
     </div>
