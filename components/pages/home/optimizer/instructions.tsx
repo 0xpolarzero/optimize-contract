644704b110208c32f78342050ffe82d3ac3287a0
polarzero <0xpolarzero@gmail.com>
2023-12-13 11:29:39 +0100

    Use Featured display instead of repo, add audit

--- components/pages/home/optimizer/instructions.tsx
+++ components/pages/home/optimizer/instructions.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 import { type FC, useMemo } from 'react';
 
+import { Check, ExternalLink, Github } from 'lucide-react';
+
 import { LIBRARIES } from '@/lib/constants/libraries';
 import { RecommendedContract, RecommendedLibrary } from '@/lib/types/library';
 
-import FeaturedRepoFeature from '@/components/templates/featured-repo';
+import CategoryTag, { Category } from '@/components/templates/category-tag';
+import FeatureDisplay from '@/components/templates/feature-display';
+import { Badge, Button, Tooltip } from '@/components/ui';
 
 // -----------------------------------------------------------------------------
 // Props
@@ -44,27 +48,68 @@ const Instructions: FC<InstructionsProps> = ({ recommendations }) => {
           console.log('items', recs);
           return (
             <div key={libraryName} className="flex flex-col space-y-2">
-              <FeaturedRepoFeature
+              <FeatureDisplay
+                className="col-span-2 w-full"
                 name={library.name}
-                description={library.instructions || ''}
-                details={library.description || ''}
-                url={library.url}
-              />
-              <div className="ml-4 flex flex-col space-y-1 text-sm text-gray-11">
-                {recs.map((rec, i) => (
-                  <div key={i}>
-                    <Link
-                      className="underline"
-                      href={rec.url}
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      {rec.name}
-                    </Link>
-                    : {rec.description}
+                description={library.description || ''}
+                symbol={<Github />}
+                button={
+                  <Button
+                    size="sm"
+                    variant="secondary"
+                    intent="primary"
+                    href={library.url}
+                    rightIcon={<ExternalLink />}
+                    newTab
+                  >
+                    Open on GitHub
+                  </Button>
+                }
+                tags={[
+                  <CategoryTag
+                    key={library.author.name}
+                    size="md"
+                    category={library.author.name as Category}
+                    url={library.author.url}
+                  />,
+                ]}
+              >
+                <div className="flex h-full flex-col space-y-4 p-4 text-[0.92rem]">
+                  {library.audit ? (
+                    <div className="flex items-center space-x-2 text-sm text-gray-11">
+                      <Tooltip content={`Audited by/on ${library.audit.authors.join(', ')}`}>
+                        <Badge variant="secondary" intent="success">
+                          <Link
+                            className="flex items-center space-x-2 hover:underline"
+                            href={library.audit.url}
+                            rel="noopener noreferrer"
+                            target="_blank"
+                          >
+                            <Check size={16} />
+                            <span>Audited</span>
+                          </Link>
+                        </Badge>
+                      </Tooltip>
+                    </div>
+                  ) : null}
+                  <div className="text-gray-12">{library.instructions || ''}</div>
+                  <div className="flex flex-grow flex-col space-y-1 text-sm text-gray-11">
+                    {recs.map((rec, i) => (
+                      <div key={i}>
+                        <Link
+                          className="underline"
+                          href={rec.url}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                        >
+                          {rec.name}
+                        </Link>
+                        : {rec.description}
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
+                </div>
+              </FeatureDisplay>
             </div>
           );
         })}
