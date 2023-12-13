736b6187490c86c3235037a039b2d65415acb5ce
polarzero <0xpolarzero@gmail.com>
2023-12-13 10:56:52 +0100

    Remove immersive bg from old project

--- components/templates/feature-display.tsx
+++ components/templates/feature-display.tsx
@@ -1,9 +1,6 @@
 import { useRouter } from 'next/navigation';
 import { type FC, type ReactNode, useState } from 'react';
 
-import { ArrowRight } from 'lucide-react';
-
-import { useImmersiveBg } from '@/lib/stores/useImmersiveBg';
 import { cn } from '@/lib/utils';
 
 // -----------------------------------------------------------------------------
@@ -39,12 +36,9 @@ const FeatureDisplay: FC<FeatureDisplayProps> = ({
   internalLink,
   addUrl,
   bgBase = 'bg-gray',
-  bgImmersive = 'bg-white',
 }) => {
   const router = useRouter();
   const [hovered, hover] = useState(false);
-
-  const { immersiveBg } = useImmersiveBg((state) => ({ immersiveBg: state.enabled }));
 
   const handleClick = () => {
     if (addUrl) {
@@ -58,9 +52,6 @@ const FeatureDisplay: FC<FeatureDisplayProps> = ({
     <div
       className={cn(
         'flex h-max w-64 flex-col overflow-hidden rounded-xl border border-gray-6 md:h-full',
-        immersiveBg
-          ? `${bgImmersive}-9/20 bg-clip-padding backdrop-blur-[2px] transition-all duration-100 hover:backdrop-blur-md`
-          : '',
         className,
       )}
     >
@@ -68,7 +59,7 @@ const FeatureDisplay: FC<FeatureDisplayProps> = ({
       <div
         className={cn(
           'relative flex h-[4.5rem] cursor-pointer items-center space-x-2.5 border-b border-gray-7 px-4 transition duration-200 ease-in-out',
-          !immersiveBg ? (hovered ? `${bgBase}-3` : `${bgBase}-2`) : '',
+          hovered ? `${bgBase}-3` : `${bgBase}-2`,
         )}
         onPointerEnter={() => hover(true)}
         onPointerLeave={() => hover(false)}
@@ -82,15 +73,6 @@ const FeatureDisplay: FC<FeatureDisplayProps> = ({
         <div>
           <div className="line-clamp-1 text-ellipsis font-medium text-gray-12">{name}</div>
           <div className="line-clamp-1 text-ellipsis text-sm text-gray-11">{description}</div>
-          {immersiveBg ? (
-            <ArrowRight
-              className={cn(
-                'absolute right-4 top-1/2 -translate-y-1/2 opacity-0 transition-opacity duration-100 md:block',
-                hovered ? 'opacity-12' : '',
-              )}
-              size={16}
-            />
-          ) : null}
         </div>
       </div>
 
@@ -98,7 +80,7 @@ const FeatureDisplay: FC<FeatureDisplayProps> = ({
       <div
         className={cn(
           'w-full grow cursor-pointer transition duration-200 ease-in-out',
-          !immersiveBg ? (hovered ? `${bgBase}-3` : `${bgBase}-2`) : '',
+          hovered ? `${bgBase}-3` : `${bgBase}-2`,
         )}
         onPointerEnter={() => hover(true)}
         onPointerLeave={() => hover(false)}
@@ -112,7 +94,7 @@ const FeatureDisplay: FC<FeatureDisplayProps> = ({
         <div
           className={cn(
             'flex h-min flex-wrap items-center justify-between border-t border-gray-6 p-2 transition duration-200 ease-in-out sm:flex-nowrap',
-            !immersiveBg ? (hovered ? `${bgBase}-3` : `${bgBase}-2`) : '',
+            hovered ? `${bgBase}-3` : `${bgBase}-2`,
           )}
           style={{ gap: 8 }}
         >
--- components/templates/featured-repo.tsx
+++ components/templates/featured-repo.tsx
@@ -2,9 +2,6 @@ import type { FC } from 'react';
 
 import { ExternalLink, Github } from 'lucide-react';
 
-import { useImmersiveBg } from '@/lib/stores/useImmersiveBg';
-import { cn } from '@/lib/utils';
-
 import FeatureDisplayMinimal from '@/components/templates/feature-display-minimal';
 import { Button } from '@/components/ui';
 
@@ -24,23 +21,23 @@ type FeaturedRepoFeatureProps = {
 // -----------------------------------------------------------------------------
 
 const FeaturedRepoFeature: FC<FeaturedRepoFeatureProps> = ({ name, description, details, url }) => {
-  const immersiveBg = useImmersiveBg((state) => state.enabledWithConditions);
-
   return (
     <FeatureDisplayMinimal
-      className={cn(
-        'col-span-2 w-full min-[960px]:col-span-4',
-        immersiveBg
-          ? 'bg-white/0 bg-clip-padding backdrop-blur-[2px] transition-all duration-100 hover:backdrop-blur-sm'
-          : 'bg-gray-2',
-      )}
+      className="col-span-2 w-full bg-gray-2 min-[960px]:col-span-4"
       name={name}
       description={description}
       details={details}
       symbol={<Github />}
       button={
-        <Button size="sm" rightIcon={<ExternalLink />} href={url} newTab>
-          View
+        <Button
+          size="sm"
+          variant="secondary"
+          intent="primary"
+          rightIcon={<ExternalLink />}
+          href={url}
+          newTab
+        >
+          Open on GitHub
         </Button>
       }
       allowWrap
