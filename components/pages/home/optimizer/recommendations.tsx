8384265ab0d2119a4f700673c58a82478791bd68
polarzero <0xpolarzero@gmail.com>
2023-12-13 12:17:14 +0100

    Use updatedCount to know if any change

--- components/pages/home/optimizer/recommendations.tsx
+++ components/pages/home/optimizer/recommendations.tsx
@@ -61,7 +61,7 @@ const Recommendations: FC<RecommendationsProps> = ({ input }) => {
     processLines();
   }, [lines]);
 
-  if (!updatedLines.length) {
+  if (!updatedCount) {
     return <div className="text-gray-11">No recommendations to show</div>;
   }
 
