ba5d2a98ba33bd705984e3814617d13feacd9742
polarzero <0xpolarzero@gmail.com>
2023-12-13 14:54:45 +0100

    Update footer

--- components/layouts/base.tsx
+++ components/layouts/base.tsx
@@ -26,8 +26,17 @@ const BaseLayout: FC<BaseLayoutProps> = ({ title, subtitle, pageSlug, children }
       <NavBar selected={pageSlug} />
       <main className="relative flex grow flex-col">{children}</main>
       <footer className="bottom-0 w-full border-t border-gray-4">
-        <div className=" py-2 text-center text-sm text-gray-11">
-          Design shamelessly forked and modified from{' '}
+        <div className=" py-2 text-center text-[0.8rem] text-gray-11">
+          by{' '}
+          <Link
+            className="font-medium text-gray-12 hover:underline"
+            href="https://twitter.com/0xpolarzero"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            polarzero
+          </Link>
+          , design forked from{' '}
           <Link
             className="font-medium text-gray-12 hover:underline"
             href="https://fiveoutofnine.com"
